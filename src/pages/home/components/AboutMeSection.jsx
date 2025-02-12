2704beeea11cabfbd3cee0f7b32394d5f30ca5d5
Ezequiel Arevalo <ezequiel.thomas.arevalo@gmail.com>
2025-02-12 13:53:45 -0300

    styles: ajusting sections spacing!

--- src/pages/home/components/AboutMeSection.jsx
+++ src/pages/home/components/AboutMeSection.jsx
@@ -3,7 +3,7 @@ import { experience, education } from "@/utilities/timeline";
 
 const AboutMeSection = () => {
   return (
-    <section id="about-me" className="min-h-screen text-white flex flex-col items-center justify-center px-6 scroll-mt-24 my-10 sm:my-16 lg:my-24">
+    <section id="about-me" className="min-h-screen text-white flex flex-col items-center justify-center px-6 scroll-mt-24 my-28 sm:my-16 lg:my-24">
       <h2 className="text-3xl sm:text-4xl font-bold mb-4">Sobre Mí</h2>
       <p className="text-lg text-neutral-400 mb-6">
         Apasionado por la tecnología y el desarrollo web, siempre buscando nuevas formas de innovar y mejorar.
--- src/pages/home/components/StatsSection.jsx
+++ src/pages/home/components/StatsSection.jsx
@@ -1,6 +1,6 @@
 const StatsSection = () => {
     return (
-      <section className="min-h-[50vh] flex flex-col items-center text-center my-10 sm:my-16 lg:my-24">
+      <section className="min-h-[50vh] flex flex-col items-center text-center my-28 sm:my-16 lg:my-24">
         <h2 className="text-3xl sm:text-4xl font-bold mb-4">Mis Estadísticas</h2>
         <p className="text-lg sm:text-xl text-neutral-400 mb-8">
             Proyectos y experiencia en desarrollo web
